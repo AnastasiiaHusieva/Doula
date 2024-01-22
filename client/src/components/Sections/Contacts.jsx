40bc4f35a95cc3e589be865ebeee373e673992de
LeoD3v <leoargusa1991@gmail.com>
2024-01-22 01:52:19 +0100

    background contact

--- client/src/components/Sections/Contacts.jsx
+++ client/src/components/Sections/Contacts.jsx
@@ -1,7 +1,40 @@
 function Contacts() {
   return (
-    <div id='Contacts'>
-      </div>
+    <div id="Contacts">
+      <h2>Contacts</h2>
+      <form className="p-2 bg-beige-dark rounded">
+        <div className="flex flex-row justify-between ">
+          <input
+            className="bg-beige-light  "
+            type="text"
+            placeholder="  Name"
+          ></input>
+          <input
+            className="bg-beige-light"
+            type="email"
+            placeholder="  Email"
+          ></input>
+        </div>
+        <div className="p-2">
+          <input
+            className="bg-beige-light"
+            type="text"
+            placeholder="  Betreff"
+          ></input>
+        </div>
+        <div>
+          <textarea
+            name="message"
+            placeholder="  Hier du kannst etwas schreiben"
+            rows="6"
+            className="w-[300px] bg-beige-light"
+          ></textarea>
+          <div>
+            <button>ABSENDEN</button>
+          </div>
+        </div>
+      </form>
+    </div>
   );
 }
 
