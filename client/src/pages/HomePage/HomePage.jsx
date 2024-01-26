ed66fb9d7e0a84c6a613b38e7e5a234bef13d840
Anastasiia <anastasiaaaguseva@gmail.com>
2024-01-27 00:51:54 +0100

    4 files

--- client/src/pages/HomePage/HomePage.jsx
+++ client/src/pages/HomePage/HomePage.jsx
@@ -1,9 +1,43 @@
 import "./HomePage.css";
+import Navbar from "./../../components/Navbar/Navbar";
+import Footer from "./../../components/Footer/Footer";
+import Banner from '../../components/Sections/Banner'
+import Contacts from './../../components/Sections/Contacts'
+import Home from '../../components/Sections/Home'
+import Wegbegleitung from '../../components/Sections/Wegbegleitung'
+import Schwangerschaft from '../../components/Sections/Schwangerschaft'
+import { useState } from "react";
 
 function HomePage() {
+  const [element, setElement] = useState('')
+ 
+  const scrollToComponent = (id) => {
+    console.log(id)
+    console.log(document)
+    const element = document.getElementById(id);
+    console.log(element)
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+      console.log('scroll bitch2')
+    }
+  };
+
+  const handleScrollClick = (id) => {
+ 
+    if (scrollToComponent) {
+      scrollToComponent(id);
+      setElement(id)
+    }
+  };
   return (
-    <div>
-      
+    <div className="bg-white">
+       <Navbar scrollToComponent={scrollToComponent} />
+      <Banner id='Banner'/>
+      <Home id='Home'/>
+      <Wegbegleitung/>
+      <Schwangerschaft/>
+      <Contacts id='Contacts'/>
+      <Footer/>
     </div>
   );
 }
